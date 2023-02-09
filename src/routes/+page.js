7423c68158a639d1e7552fea3e44dbeffeb6fa00
consultingninja <consultingninja.tech@gmail.com>
2023-02-09 11:45:36 -0600

    fix extra property

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -53,6 +53,6 @@ export async function load({fetch}){
     const data = await response.json()
     //console.log(data);
     return{
-        ballInfo: data
+        data
     }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,6 @@
 
 //https://svelte-ball.vercel.app
      function getSet(topRegBalls,topPowerBalls){
-        console.log("hello? wtf", topRegBalls,topPowerBalls)
-        console.log(data);
         let set = [];
         if(!topRegBalls || !topPowerBalls)return 
 
