baff690bcd4fca90b44e292f62df354efbbcf163
Mitchell Migala <mitchellmigala@Mitchells-MacBook-Pro.local>
2024-04-29 18:58:07 -0500

    create and add small nav

---
+++ src/lib/nav.svelte
@@ -0,0 +1,23 @@
+<nav class="navbar">
+    <a href="/">Home</a>
+    <a href="/claude">Claude</a>
+</nav>
+
+<style>
+    .navbar {
+        position: sticky;
+        top: 0;
+        background-color: #333;
+        padding: 10px;
+        display: flex;
+        justify-content: center;
+    }
+
+    .navbar a {
+        color: white;
+        font-size: 20px;
+        margin: 0 10px;
+        text-decoration: none;
+    }
+</style>
+
